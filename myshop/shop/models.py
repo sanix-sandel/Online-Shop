4db0fb0cbeb84dfca48b86842e0e752445dd690c
sanix sandel <sanicksikani@gmail.com>
2020-04-16 15:30:08 +0300

    building catalog views

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Category(models.Model):
     name=models.CharField(max_length=200,
@@ -6,12 +7,16 @@ class Category(models.Model):
     slug=models.SlugField(max_length=200,
                         unique=True)
     class Meta:
-        ordering=('name')
+        ordering=('name',)
         verbose_name='category'
         verbose_name_plural='categories'
     
     def __str__(self):
         return self.name                             
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category',
+                        args=[self.slug])
 
 
 class Product(models.Model):
@@ -32,4 +37,8 @@ class Product(models.Model):
         index_together=(('id', 'slug'),)
 
     def __str__(self):
-        return self.name    
+        return self.name  
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail',
+                        args=[self.id, self.slug])      
