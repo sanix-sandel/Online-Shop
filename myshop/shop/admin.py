f57e09846dd98836d9b72e0e4a1b8419b6d9fdcc
sanix sandel <sanicksikani@gmail.com>
2020-04-16 16:56:56 +0300

    creating catalog templates

--- myshop/shop/admin.py
+++ myshop/shop/admin.py
@@ -6,6 +6,8 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display=['name', 'slug']
     prepopulated_fields={'slug':('name',)}
 
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display=['name', 'slug', 'price',
