53632764c880fa19704d74b40960bbd10f56a66f
sanix sandel <sanicksikani@gmail.com>
2020-07-06 18:26:36 +0300

    braintree integration

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -21,9 +21,8 @@ def order_create(request):
             #Clear the cart
             cart.clear()
             order_created.delay(order.id)
-            request.session['order_id']=order.id
-
-            return redirect(reverse('payment:process'))
+            return render(request, 'orders/order/created.html',
+                            {'order':order})
     else:#Get request instanciates the OrderCreateForm form and renders the
         #orders/order/create.html
         form=OrderCreateForm()
